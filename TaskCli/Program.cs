97ffc27f529e2f5fc1d58e3301a3bdc1fd0ad190
runarer <95697611+runarer@users.noreply.github.com>
2026-01-16 12:05:20 +0100

    First attempt to connect to google

--- TaskCli/Program.cs
+++ TaskCli/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Google.Apis.Services;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Tasks.v1;
+using Google.Apis.Tasks.v1.Data;
+using Google.Apis.Util.Store;
+
+
+UserCredential credential;
+
+// Create credentials and authorize
+// TODO: If using file for secrets we should check if it exists and exit gracefully if not with and error message
+using var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read);
+credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+    GoogleClientSecrets.FromStream(stream).Secrets,
+    [TasksService.Scope.Tasks],
+    "user",
+    CancellationToken.None,
+    new FileDataStore("Tasks.Auth.Store")).Result;
+// Timeout?
+// Cancel?
+// Exceptions?
+
+// Create service
+var taskService = new TasksService(new BaseClientService.Initializer()
+{
+    HttpClientInitializer = credential,
+    ApplicationName = "TaskCli",
+});
+
+// Get Tasklists
+TaskLists results = taskService.Tasklists.List().Execute();
+
+foreach (TaskList list in results.Items)
+{
+    Console.WriteLine("\t\t" + list.Title);
+}
