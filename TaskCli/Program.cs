d1664a04a1aec7f365bc7003d6fb5d007d47edf7
runarer <95697611+runarer@users.noreply.github.com>
2026-01-20 09:30:20 +0100

    En test model

--- TaskCli/Program.cs
+++ TaskCli/Program.cs
@@ -3,46 +3,60 @@ using Google.Apis.Auth.OAuth2;
 using Google.Apis.Tasks.v1;
 using Google.Apis.Tasks.v1.Data;
 using Google.Apis.Util.Store;
+using Spectre.Console;
 
 
-UserCredential credential;
+using TaskCli.Models;
+
+TaskCli.Models.TaskList tasklist = new()
+{
+    Name = "Testlist",
+    Tasks = [new() { Title = "En task" }, new() { Title = "En annen task" }]
+};
+
+
+// UserCredential credential;
 
 // Create credentials and authorize
 // TODO: If using file for secrets we should check if it exists and exit gracefully if not with and error message
-using var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read);
-credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-    GoogleClientSecrets.FromStream(stream).Secrets,
-    [TasksService.Scope.Tasks],
-    "user",
-    CancellationToken.None,
-    new FileDataStore("Tasks.Auth.Store")).Result;
+// using var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read);
+// credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+//     GoogleClientSecrets.FromStream(stream).Secrets,
+//     [TasksService.Scope.Tasks],
+//     "user",
+//     CancellationToken.None,
+//     new FileDataStore("Tasks.Auth.Store")).Result;
 // Timeout?
 // Cancel?
 // Exceptions?
 
 // Create service
-var taskService = new TasksService(new BaseClientService.Initializer()
-{
-    HttpClientInitializer = credential,
-    ApplicationName = "TaskCli",
-});
+// var taskService = new TasksService(new BaseClientService.Initializer()
+// {
+//     HttpClientInitializer = credential,
+//     ApplicationName = "TaskCli",
+// });
 
 // Get Tasklists
-TaskLists results = taskService.Tasklists.List().Execute();
+// TaskLists results = taskService.Tasklists.List().Execute();
 
-foreach (TaskList list in results.Items)
-{
-    Console.WriteLine(list.Title);
-    var tasksRequest = taskService.Tasks.List(list.Id);
-    // Både ShowCompleted and ShowHidden må være true, Maxresult er valgfritt.
-    tasksRequest.ShowCompleted = true;
-    tasksRequest.ShowHidden = true;
-    tasksRequest.MaxResults = 100;
+// foreach (TaskList list in results.Items)
+// {
+//     Console.WriteLine(list.Title);
+//     var tasksRequest = taskService.Tasks.List(list.Id);
+//     // Både ShowCompleted and ShowHidden må være true, Maxresult er valgfritt.
+//     tasksRequest.ShowCompleted = true;
+//     tasksRequest.ShowHidden = true;
+//     tasksRequest.MaxResults = 100;
 
-    Tasks tasks = tasksRequest.Execute();
-    foreach (Google.Apis.Tasks.v1.Data.Task task in tasks.Items)
-    {
-        Console.WriteLine($"\t{(task.Parent is not null ? "\t" : "")}{((task.Completed is null) ? "O" : "X")} {task.Title}");
+//     Tasks tasks = tasksRequest.Execute();
+//     foreach (Google.Apis.Tasks.v1.Data.Task task in tasks.Items)
+//     {
+//         Console.WriteLine($"\t{(task.Parent is not null ? "\t" : "")}{((task.Completed is null) ? "O" : "X")} {task.Title}");
 
-    }
-}
+//     }
+// }
+
+// Add a Task
+// Google.Apis.Tasks.v1.Data.Task newTask = taskService.Tasks.Insert(new Google.Apis.Tasks.v1.Data.Task { Title = "Added from CLI then moves to other list" }, results.Items[0].Id).Execute();
+// taskService.Tasks.Move(results.Items[1].Id, newTask.Id).Execute();
