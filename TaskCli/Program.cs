e8d90c5233b2ad9e5edbadf33e0656aba0b11ef7
runarer <95697611+runarer@users.noreply.github.com>
2026-01-26 11:53:39 +0100

    Simple tests

--- TaskCli/Program.cs
+++ TaskCli/Program.cs
@@ -1,13 +1,32 @@
 ﻿
 
 
+using TaskCli.Model;
+
 namespace TaskCli;
 
 public class TaskCli
 {
-    public async static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         IToDoListsStorage todoStorage = new Services.GoogleTaskToModel("client_secret.json");
+
+        // Test if I get something back
+
+        var lists = await todoStorage.GetLists();
+
+        foreach (var l in lists)
+            Console.WriteLine(l);
+
+        var list = await todoStorage.GetList(lists[1]);
+        foreach (var l in list.Items)
+        {
+            Console.WriteLine(l.Title);
+            if (l.SubItems.Count > 0)
+                foreach (var subItem in l.SubItems)
+                    Console.WriteLine($"\t{subItem.Title}");
+        }
+
     }
 }
 
