bb10ce165086b3c0f14658e6ca376bfa23587936
runarer <95697611+runarer@users.noreply.github.com>
2026-01-26 10:39:06 +0100

    Clean up main

--- TaskCli/Program.cs
+++ TaskCli/Program.cs
@@ -1,60 +1,15 @@
-﻿using Google.Apis.Services;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Tasks.v1;
-using Google.Apis.Tasks.v1.Data;
-using Google.Apis.Util.Store;
-using Spectre.Console;
+﻿
 
 
+namespace TaskCli;
 
-// TaskCli.Models.TaskList tasklist = new()
-// {
-//     Name = "Testlist",
-//     Tasks = [new() { Title = "En task" }, new() { Title = "En annen task" }, new() { Title = "En task" }, new() { Title = "En annen task" }]
-// };
-
-UserCredential credential;
-
-// Create credentials and authorize
-// TODO: If using file for secrets we should check if it exists and exit gracefully if not with and error message
-using var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read);
-credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-    GoogleClientSecrets.FromStream(stream).Secrets,
-    [TasksService.Scope.Tasks],
-    "user",
-    CancellationToken.None,
-    new FileDataStore("Tasks.Auth.Store")).Result;
-// Timeout?
-// Cancel?
-// Exceptions?
-
-// Create service
-var taskService = new TasksService(new BaseClientService.Initializer()
+public class TaskCli
 {
-    HttpClientInitializer = credential,
-    ApplicationName = "TaskCli",
-});
-
-// Get Tasklists
-TaskLists results = taskService.Tasklists.List().Execute();
-
-foreach (TaskList list in results.Items)
-{
-    Console.WriteLine(list.Title);
-    var tasksRequest = taskService.Tasks.List(list.Id);
-    // Både ShowCompleted and ShowHidden må være true, Maxresult er valgfritt.
-    tasksRequest.ShowCompleted = true;
-    tasksRequest.ShowHidden = true;
-    tasksRequest.MaxResults = 100;
-
-    Tasks tasks = tasksRequest.Execute();
-    foreach (Google.Apis.Tasks.v1.Data.Task task in tasks.Items)
+    public async static void Main(string[] args)
     {
-        Console.WriteLine($"\t{(task.Parent is not null ? "\t" : "")}{((task.Completed is null) ? "O" : "X")} {task.Title}");
-
+        IToDoListsStorage todoStorage = new Services.GoogleTaskToModel("client_secret.json");
     }
 }
 
-// // Add a Task
-// Google.Apis.Tasks.v1.Data.Task newTask = taskService.Tasks.Insert(new Google.Apis.Tasks.v1.Data.Task { Title = "Added from CLI then moves to other list" }, results.Items[0].Id).Execute();
-// taskService.Tasks.Move(results.Items[1].Id, newTask.Id).Execute();
+
+
