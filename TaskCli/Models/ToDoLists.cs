b22a37b32f295cafa137ad3552e7114df5ea5bb0
runarer <95697611+runarer@users.noreply.github.com>
2026-01-26 13:26:49 +0100

    Add a model class, ToDoLists
    This is the entrypoint for the model.

---
+++ TaskCli/Models/ToDoLists.cs
@@ -0,0 +1,59 @@
+namespace TaskCli.Model;
+
+public class ToDoLists
+{
+    private IToDoListsStorage _storage;
+
+    private Dictionary<string, ToDoList?> _lists = [];
+
+    private ToDoLists(IToDoListsStorage storage)
+    {
+        _storage = storage;
+
+
+    }
+
+    /// <summary>
+    /// Use this to create a new object of ToDoLists
+    /// </summary>
+    /// <param name="storage">A reference to a storage</param>
+    /// <param name="defaultList">A list to be included on construction</param>
+    /// <returns>An object of ToDoLists populated with lists, the lists are empty except for defaultList</returns>
+    /// <exception cref="InvalidOperationException">defaultList was not found in lists</exception>
+    public static async Task<ToDoLists> CreateAsync(IToDoListsStorage storage, string? defaultList = null)
+    {
+        var todoLists = new ToDoLists(storage);
+
+        var storedLists = await storage.GetLists();
+
+        foreach (var list in storedLists)
+        {
+            todoLists._lists[list] = null;
+        }
+
+        if (defaultList is not null)
+        {
+            if (!todoLists._lists.ContainsKey(defaultList))
+                throw new InvalidOperationException($"Default list {defaultList} could not be found in storage!");
+
+            todoLists._lists[defaultList] = await todoLists._storage.GetList(defaultList);
+        }
+
+        return todoLists;
+    }
+
+    public async Task AddList(string name)
+    {
+
+    }
+
+    public async Task RemoveList(string name)
+    {
+
+    }
+
+    public async Task UpdateListName(string newName)
+    {
+
+    }
+}
