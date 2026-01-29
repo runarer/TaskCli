4af94e324770b6772638263b8a8f77498ea3662c
runarer <95697611+runarer@users.noreply.github.com>
2026-01-29 11:58:00 +0100

    Lots of cool stuff added
    The app will run in two loops, a main one and one for the live display. It is in the live display that input is handled. An action enum is created and passed outside so we can switch to the right prompt after.

--- TaskCli/Program.cs
+++ TaskCli/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 
 
+using Spectre.Console;
+using Spectre.Console.Rendering;
 using TaskCli.Controller;
 using TaskCli.Model;
 using TaskCli.Views;
@@ -9,22 +11,178 @@ namespace TaskCli;
 
 public class TaskCli
 {
+
+    private static string[] _menuItems = ["Yesterday", "Today", "Tommorrow", "Select List", "Add List", "Rename List", "Delete List"];
     public static async Task Main(string[] args)
     {
 
-        var cancellationToken = new CancellationTokenSource();
-        Console.CancelKeyPress += (_, e) => { e.Cancel = true; cancellationToken.Cancel(); };
+        // var cancellationToken = new CancellationTokenSource();
+        // Console.CancelKeyPress += (_, e) => { e.Cancel = true; cancellationToken.Cancel(); };
 
-        IToDoListsStorage todoStorage = new Services.GoogleTaskToModel("client_secret.json");
-        ToDoLists toDoLists = await ToDoLists.CreateAsync(todoStorage);
+        // IToDoListsStorage todoStorage = new Services.GoogleTaskToModel("client_secret.json");
+        // ToDoLists toDoLists = await ToDoLists.CreateAsync(todoStorage);
 
-        ConsoleView consoleView = new();
+        // ConsoleView consoleView = new();
 
-        MainController controller = new(toDoLists, consoleView);
+        // MainController controller = new(toDoLists, consoleView);
 
-        await controller.RunAsync(cancellationToken.Token);
+        // await controller.RunAsync(cancellationToken.Token);
+
+
+        var layout = new Layout("Root")
+            .SplitColumns(
+                new Layout("Menu").Size(16),
+                new Layout("ToDoList").Ratio(1),
+                new Layout("Info").Ratio(1));
+
+        layout["Menu"].Update(CreateMenu(1));
+        layout["ToDoList"].Update(new Panel("2/4 width (half)").BorderColor(Color.Green).Expand());
+        layout["Info"].Update(new Panel("1/4 width").BorderColor(Color.Yellow).Expand());
+
+        // AnsiConsole.Write(layout);
+
+        /* det kan hende at vi må gå ut av live view når select list eller add/edit er valgt.
+            wrap alt i en while loop og gå inn og ut av live view etter behov.
+
+            Q -> gå ut av begge loops.
+        */
+
+        bool runApp = true;
+        int selectedItem = 1;
+        Panels selectedPanel = Panels.Menu;
+        Actions action = Actions.Quit;
+
+        while (runApp)
+        {
+            AnsiConsole.Clear();
+            AnsiConsole.Live(layout).Start(ctx =>
+            {
+                bool runLiveDisplay = true;
+
+
+                while (runLiveDisplay)
+                {
+                    // Render
+                    layout["Menu"].Update(CreateMenu(selectedPanel == Panels.Menu ? selectedItem : -1));
+                    layout["ToDoList"].Update(new Panel("2/4 width (half)").BorderColor(Color.Green).Expand());
+                    layout["Info"].Update(new Panel("1/4 width").BorderColor(Color.Yellow).Expand());
+                    ctx.Refresh();
+
+                    // Wait for input
+                    var keyPress = Console.ReadKey(intercept: true);
+
+                    // Handle input
+                    switch (keyPress.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            if (selectedPanel == Panels.Menu)
+                                selectedItem = Math.Max(0, selectedItem - 1);
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (selectedPanel == Panels.Menu)
+                                selectedItem = Math.Min(6, selectedItem + 1);
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if (selectedPanel == Panels.ToDoList)
+                                selectedPanel = Panels.Menu;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            if (selectedPanel == Panels.Menu)
+                                selectedPanel = Panels.ToDoList;
+                            break;
+                        case ConsoleKey.Enter: //If menu then select action, if ToDoList then expand/collapse subtasks
+                            if (selectedPanel == Panels.Menu)
+                            {
+                                action = GetMenuAction(_menuItems[selectedItem]);
+                                runLiveDisplay = false;
+                                break;
+                            }
+                            if (selectedPanel == Panels.ToDoList)
+                            {
+
+                            }
+                            break;
+                        case ConsoleKey.Spacebar: //Toggle completion for task
+                            break;
+                        case ConsoleKey.A: // Add a task
+                            action = Actions.AddTask;
+                            runLiveDisplay = false;
+                            break;
+                        case ConsoleKey.D: // Delete a task
+                            action = Actions.DeleteTask;
+                            break;
+                        case ConsoleKey.E: // Edit a task
+                            action = Actions.EditTask;
+                            runLiveDisplay = false;
+                            break;
+                        case ConsoleKey.I: //Indent/unindent a task
+                            break;
+                        case ConsoleKey.M: // Move a task -> select list to move to
+                            action = Actions.MoveTask;
+                            runLiveDisplay = false;
+                            break;
+                        case ConsoleKey.Q:
+                            action = Actions.Quit;
+                            runLiveDisplay = false;
+                            break;
+                    }
+                }
+            });
+            switch (action)
+            {
+                case Actions.Quit:
+                    runApp = false;
+                    break;
+            }
+        }
+        AnsiConsole.Clear();
     }
+    private static Rows CreateMenu(int selected)
+    {
+        string selectedColor = "blue";
+        string unselectedColor = "green";
+        string infoColor = "gray";
+        string infoColorKey = "white";
+
+        var item = new Rows(
+            new Rule().Border(BoxBorder.None),
+            new Markup($"[{((selected == 0) ? selectedColor : unselectedColor)}]{_menuItems[0]}[/]"),
+            new Markup($"[{((selected == 1) ? selectedColor : unselectedColor)}]{_menuItems[1]}[/]"),
+            new Markup($"[{((selected == 2) ? selectedColor : unselectedColor)}]{_menuItems[2]}[/]"),
+            new Rule().Border(BoxBorder.None),
+            new Markup($"[{((selected == 3) ? selectedColor : unselectedColor)}]{_menuItems[3]}[/]"),
+            new Markup($"[{((selected == 4) ? selectedColor : unselectedColor)}]{_menuItems[4]}[/]"),
+            new Markup($"[{((selected == 5) ? selectedColor : unselectedColor)}]{_menuItems[5]}[/]"),
+            new Markup($"[{((selected == 6) ? selectedColor : unselectedColor)}]{_menuItems[6]}[/]"),
+            new Rule().Border(BoxBorder.None),
+            new Rule().Border(BoxBorder.None),
+            new Markup($"[{infoColorKey}]<Space>[/][{infoColor}] Check[/]"),
+            new Markup($"[{infoColorKey}]A[/]dd Task[{infoColor}][/]"),
+            new Markup($"[{infoColorKey}]E[/]dit Task[{infoColor}][/]"),
+            new Markup($"[{infoColorKey}]M[/]ove Task[{infoColor}][/]"),
+            new Markup($"[{infoColorKey}]D[/]elete Task[{infoColor}][/]"),
+            new Markup($"[{infoColorKey}]Q[/][{infoColor}]uit[/]")
+        );
+
+        return item;
+    }
+    private static Actions GetMenuAction(string choice) => choice switch
+    {
+        "Select List" => Actions.SelectList,
+        "Add List" => Actions.AddList,
+        "Rename List" => Actions.RenameList,
+        "Delete List" => Actions.DeleteList,
+        _ => Actions.Quit
+    };
 }
 
 
+public enum Panels
+{
+    Menu, ToDoList, Information
+}
 
+public enum Actions
+{
+    Quit, AddTask, EditTask, MoveTask, DeleteTask, SelectList, AddList, RenameList, DeleteList,
+}
