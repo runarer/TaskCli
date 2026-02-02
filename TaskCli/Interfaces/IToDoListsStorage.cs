727305976f9e22e61dc9c123accd45b361c4966f
runarer <95697611+runarer@users.noreply.github.com>
2026-02-02 10:08:00 +0100

    Added Add, Remove, Update list

--- TaskCli/Interfaces/IToDoListsStorage.cs
+++ TaskCli/Interfaces/IToDoListsStorage.cs
@@ -6,4 +6,7 @@ public interface IToDoListsStorage
 {
     public Task<List<string>> GetLists();
     public Task<ToDoList> GetList(string listName);
+    public Task AddList(ToDoList list);
+    public Task RemoveList(ToDoList list);
+    public Task UpdateList(ToDoList oldList, ToDoList newList);
 }
