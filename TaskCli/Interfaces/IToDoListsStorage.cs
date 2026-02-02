87953f69a6884973640f670b0647a7c64c46576f
runarer <95697611+runarer@users.noreply.github.com>
2026-02-02 10:42:52 +0100

    Change to string insted of ToDoList

--- TaskCli/Interfaces/IToDoListsStorage.cs
+++ TaskCli/Interfaces/IToDoListsStorage.cs
@@ -7,6 +7,6 @@ public interface IToDoListsStorage
     public Task<string[]> GetLists();
     public Task<ToDoList> GetList(string listName);
     public Task AddList(ToDoList list);
-    public Task RemoveList(ToDoList list);
-    public Task UpdateList(ToDoList oldList, ToDoList newList);
+    public Task DeleteList(string list);
+    public Task UpdateList(string oldList, string newList);
 }
