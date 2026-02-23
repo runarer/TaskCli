f68b63a03c12f5b6be6575f1ad927fad6107e9fd
runarer <95697611+runarer@users.noreply.github.com>
2026-02-23 13:27:49 +0100

    Removes Expanded and adds doc string
    The Expanded property was not needed as it's not the renderers job to handle collapsed lists.

--- TaskCli/Views/RenderListItem.cs
+++ TaskCli/Views/RenderListItem.cs
@@ -2,4 +2,9 @@ using TaskCli.Model;
 
 namespace TaskCli.Views;
 
-public record RenderListItem(ToDoItem Item, int Indent = 0, bool Bxpanded = false);
+/// <summary>
+/// A record for sending information needed for the rendering
+/// </summary>
+/// <param name="Item">The ToDoItem to render</param>
+/// <param name="Indent">If and how many sublevels an item is</param>
+public record RenderListItem(ToDoItem Item, int Indent = 0);
